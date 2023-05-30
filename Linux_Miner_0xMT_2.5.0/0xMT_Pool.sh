d7830463c7b01059d0a2e8a7cafb5f4505cd4fbb
Mineable Token (0xMT) <133748763+MineableToken@users.noreply.github.com>
2023-05-29 22:13:30 -0500

    Update 0xMT_Pool.sh

--- Linux_Miner_0xMT_2.5.0/0xMT_Pool.sh
+++ Linux_Miner_0xMT_2.5.0/0xMT_Pool.sh
@@ -11,6 +11,6 @@ while : ; do
   if [ -f 0xMineableTokenMiner.conf ] ; then
     rm -f 0xMineableTokenMiner.conf
   fi
-  dotnet 0xMineableTokenMiner.dll abiFile=0xMT.abi contract=0xAe56c981F9bb8b07E380B209FcD1498c5876Fd4c web3api=https://arb-mainnet.g.alchemy.com/v2/dKEjBE4wIqKjLWur73EIvs8CqUH36oBw pool=http://abastoken.org:8080 address=0x9172ff7884CEFED19327aDaCe9C470eF1796105c
+  dotnet 0xMineableTokenMiner.dll abiFile=0xMT.abi contract=0xAe56c981F9bb8b07E380B209FcD1498c5876Fd4c web3api=https://rpc.ankr.com/arbitrum pool=http://pool.0xmt.com:8989 address=0x9172ff7884CEFED19327aDaCe9C470eF1796105c
   [[ $? -eq 22 ]] || break
 done
